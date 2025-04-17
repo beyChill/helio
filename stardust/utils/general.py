5f3727f9e2162964e1f71e8bf3270538c5fdc75a
beyChill <74477178+beyChill@users.noreply.github.com>
2025-04-17 18:13:49 -0400

    new function

--- stardust/utils/general.py
+++ stardust/utils/general.py
@@ -1,13 +1,34 @@
 from datetime import datetime, timedelta
 from random import uniform
 from typing import Any
+from stardust.apps import __apps__ as helio_apps
 
 import m3u8
+from rnet import Client, Response
+import stardust
 from stardust.apps.chaturbate.db_write import write_cb_url
-from stardust.utils.applogging import AppLogger
-# from stardust.utils.applogging import AppLogger
+from stardust.utils.applogging import HelioLogger, loglvl
 
-log = AppLogger()
+log = HelioLogger()
+
+
+def get_app_name(app_tag: str):
+    """
+    Accept input convert to a site abbreviation and name
+        Cmd2 argparse decorators complete error checking
+        Therfore no input error checking is within this fuction
+    """
+    APP_DICT = {}
+    for app in dir(helio_apps):
+        if app.startswith("app_") and not app.startswith("__"):
+            app_data = getattr(helio_apps, app)
+            APP_DICT[app_data[0]] = app_data[1]
+
+    INVERSE_DICT = {v: k for k, v in APP_DICT.items()}
+    app_name: str = APP_DICT.get(app_tag, None)
+
+    slug: str = app_name if app_name in APP_DICT else INVERSE_DICT.get(app_name, None)
+    return (slug, app_name)
 
 
 def script_delay(min: float, max: float):
@@ -35,3 +56,41 @@ def process_hls(results: list[Any]):
         write_cb_url(write_data)
 
     return streamer_url
+
+
+async def check_helio_github_version():
+    """Check Helio version currency"""
+
+    log.info("Checking Helio version")
+    repo_path = stardust.__repo_path__
+    client = Client()
+    latest_version = ""
+    try:
+        headers = {
+            "user-agent": "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0",
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
+
+        resp: Response = await client.get(
+            f"https://api.github.com/repos/{repo_path}/releases/latest",
+            headers=headers,
+        )
+
+        github_json = await resp.json()
+        latest_version = github_json["name"]
+
+    except Exception as e:
+        log.error("Helio version check failed")
+        log.error(e)
+        return False
+
+    if stardust.__version__ < latest_version:
+        log.warning(f"Helio version {stardust.__version__} is not current")
+        log.warning(f"Consider updating to {latest_version}")
+        return False
+
+    if stardust.__version__ >= latest_version:
+        log.app(loglvl.SUCCESS, "Helio is current")
+
+    return True
