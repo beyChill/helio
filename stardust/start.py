a8c9f5349f04651c679ac91c7113d622b4efb51e
beyChill <74477178+beyChill@users.noreply.github.com>
2025-04-19 01:13:02 -0400

    updated cli name to match previously changed name

--- stardust/start.py
+++ stardust/start.py
@@ -1,10 +1,9 @@
-
 import sys
 from threading import Thread
-from stardust.apps.chaturbate.api_streamer_online import run_cb_streamers
-from stardust.apps.chaturbate.api_get_jsons import loop_cb_room_list
 
-from stardust.cli.commandline import CLI
+from stardust.apps.chaturbate.api_get_jsons import loop_cb_room_list
+from stardust.apps.chaturbate.api_streamer_online import run_cb_streamers
+from stardust.cli.commandline import HelioCli
 from stardust.database.db_base import db_init
 
 threads = [
@@ -13,10 +12,10 @@ threads = [
 ]
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     db_init()
     for thread in threads:
         thread.start()
 
-    cli = CLI()
+    cli = HelioCli()
     sys.exit(cli.cmdloop())
