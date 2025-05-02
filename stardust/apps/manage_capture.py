ee18377ab43c84c8f13ac023b809afe42c34fdcb
beyChill <74477178+beyChill@users.noreply.github.com>
2025-05-02 19:27:20 -0400

    refactor: for scale and moved location

---
+++ stardust/apps/manage_capture.py
@@ -0,0 +1,20 @@
+from stardust.ffmpeg_files.ffmpeg_data import FFmpegConfig
+from stardust.ffmpeg_files.ffmpeg_saver import CaptureStreamer
+
+
+def start_capture(data: list | tuple):
+    """
+    Compile data for capture
+
+    Capture using ffmpeg
+    """
+    if isinstance(data, list):
+        config_list = [
+            FFmpegConfig(name_, slug, url_).return_data for name_, slug, url_ in data
+        ]
+        _ = [CaptureStreamer(result) for result in config_list]
+
+    name_, slug, url = data
+    config = FFmpegConfig(name_, slug, url).return_data
+    CaptureStreamer(config)
+    return None
