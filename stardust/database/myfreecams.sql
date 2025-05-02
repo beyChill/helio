d3d57aef2cd83dc2b201f2d36b85d4d684fde6e3
beyChill <74477178+beyChill@users.noreply.github.com>
2025-05-02 15:02:33 -0400

    refactor: renamed / added for app scaling

--- stardust/database/myfreecams.sql
+++ stardust/database/myfreecams.sql
@@ -1,35 +1,27 @@
 CREATE TABLE IF NOT EXISTS myfreecams (
-    streamer_name VARCHAR(30) NOT NULL,
-    creation    INTEGER DEFAULT NULL,
-    is_new      INTEGER DEFAULT NULL,
-    missmfc     INTEGER DEFAULT NULL,
-    camscore    INTEGER DEFAULT NULL,
-    continent   VARCHAR(30) DEFAULT NULL,
-    country     VARCHAR(4) DEFAULT NULL,
-    rank_       INTEGER DEFAULT NULL,
-    rc          INTEGER DEFAULT NULL,
-    last_broadcast DATETIME DEFAULT NULL,
-    data_review INTEGER DEFAULT NULL,
-    data_keep INTEGER DEFAULT NULL,
-    data_total INTEGER DEFAULT NULL,
-    last_capture DATETIME DEFAULT NULL,
-    seek_capture DATETIME DEFAULT NULL,
-    pid_ INTEGER DEFAULT NULL,
-    url_ VARCHAR(232) DEFAULT NULL,
-    followers INTEGER DEFAULT NULL,
-    viewers INTEGER DEFAULT NULL,
-    most_viewers INTEGER DEFAULT NULL,
-    block_date DATETIME DEFAULT NULL,
-    notes TEXT,
-    tags TEXT,
-    category VARCHAR(15) DEFAULT NULL,
-    bio_chk_date DATETIME DEFAULT NULL,
-    bio_fail_date DATETIME DEFAULT NULL,
+    streamer_name   VARCHAR(30) NOT NULL,
+    last_broadcast  DATETIME DEFAULT NULL,
+    data_review     INTEGER DEFAULT NULL,
+    data_keep       INTEGER DEFAULT NULL,
+    data_total      INTEGER DEFAULT NULL,
+    last_capture    DATETIME DEFAULT NULL,
+    seek_capture    DATETIME DEFAULT NULL,
+    process_id      INTEGER DEFAULT NULL,
+    capture_url     VARCHAR(232) DEFAULT NULL,
+    followers       INTEGER DEFAULT NULL,
+    viewers         INTEGER DEFAULT NULL,
+    most_viewers    INTEGER DEFAULT NULL,
+    block_date      DATETIME DEFAULT NULL,
+    notes           TEXT,
+    tags            TEXT,
+    category        VARCHAR(15) DEFAULT NULL,
+    bio_chk_date    DATETIME DEFAULT NULL,
+    bio_fail_date   DATETIME DEFAULT NULL,
     bio_fail_status INTEGER DEFAULT NULL,
     bio_fail_detail VARCHAR(50) DEFAULT NULL,
-    bio_fail_code VARCHAR(20) DEFAULT NULL,
-    created_on DATETIME DEFAULT (date('now', 'localtime')),
-    updated_at DATETIME DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')),
+    bio_fail_code   VARCHAR(20) DEFAULT NULL,
+    created_on      DATETIME DEFAULT (date('now', 'localtime')),
+    updated_at      DATETIME DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')),
     PRIMARY KEY (streamer_name)
 );
 CREATE TRIGGER update_myfreecams_updated_at
@@ -51,6 +43,32 @@ WHERE rowid = NEW.rowid;
 END;
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_streamer ON myfreecams (streamer_name);
+
+CREATE TABLE IF NOT EXISTS streamer_data(
+    streamer_name VARCHAR(30) NOT NULL,
+    creation    INTEGER DEFAULT NULL,
+    is_new      INTEGER DEFAULT NULL,
+    missmfc     INTEGER DEFAULT NULL,
+    camscore    INTEGER DEFAULT NULL,
+    continent   VARCHAR(30) DEFAULT NULL,
+    country     VARCHAR(4) DEFAULT NULL,
+    rank_       INTEGER DEFAULT NULL,
+    rc          INTEGER DEFAULT NULL,
+    created_on  DATETIME DEFAULT (date('now', 'localtime')),
+    updated_at  DATETIME DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')),
+    PRIMARY KEY (streamer_name)
+)
+
+CREATE TRIGGER update_streamer_updated_at
+AFTER
+UPDATE ON streamer_data
+    WHEN old.updated_at <> (datetime(CURRENT_TIMESTAMP, 'localtime')) BEGIN
+UPDATE streamer_data
+SET updated_at = (datetime(CURRENT_TIMESTAMP, 'localtime'))
+WHERE streamer_name = OLD.streamer_name;
+END;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_streamer ON streamer_data (streamer_name);
+
 
 CREATE TABLE IF NOT EXISTS url_data (
     streamer_name VARCHAR(30) NOT NULL,
