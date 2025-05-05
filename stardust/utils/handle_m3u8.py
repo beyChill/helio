4f491c12c2db12204f84bb2313d0a8dabaa6a4f7
beyChill <74477178+beyChill@users.noreply.github.com>
2025-05-05 01:41:31 -0400

    Bug: handled 403 error. MFC private will respond with http code 403

--- stardust/utils/handle_m3u8.py
+++ stardust/utils/handle_m3u8.py
@@ -1,28 +1,32 @@
+from urllib.error import HTTPError
+
 import m3u8
 
 from stardust.utils.applogging import HelioLogger
 
 log = HelioLogger()
 
-class HandleM3u8:
 
-    def __init__(self, url:str):
+class HandleM3u8:
+    def __init__(self, url: str):
         self.url = url
         self.m3u8 = self.load()
 
         if not self.m3u8:
             return
 
-        self.top_bandwidth=self.get_top_bandwidth()
-        self.cb_m3u8=self.new_cb_m3u8()
-        self.mfc_m3u8=self.new_mfc_m3u8()
+        self.top_bandwidth = self.get_top_bandwidth()
+        self.cb_m3u8 = self.new_cb_m3u8()
+        self.mfc_m3u8 = self.new_mfc_m3u8()
 
     def load(self):
-        data = m3u8.load(self.url)
-        if not data.is_variant:
-            log.error("The m3u8 does not contain variant playlists")
+        data = ""
+        try:
+            data = m3u8.load(self.url)
+            return data
+        except HTTPError as e:
+            log.warning(f"{e}")
             return None
-        return data
 
     def get_top_bandwidth(self):
         if not self.m3u8:
@@ -35,14 +39,12 @@ class HandleM3u8:
             else False,
         )
         return top_bw.uri
-    
+
     def new_cb_m3u8(self):
-        new_m3u8 = self.url.replace("playlist.m3u8",str(self.top_bandwidth))
+        new_m3u8 = self.url.replace("playlist.m3u8", str(self.top_bandwidth))
         return new_m3u8
-    
+
     def new_mfc_m3u8(self):
-        split_m3u8=self.url.split('/').pop()
-        new_url = self.url.replace(split_m3u8,str(self.top_bandwidth))
+        split_m3u8 = self.url.split("/").pop()
+        new_url = self.url.replace(split_m3u8, str(self.top_bandwidth))
         return new_url
-    
- 
