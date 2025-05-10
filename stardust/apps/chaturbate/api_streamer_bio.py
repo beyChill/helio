0657217145620accb5a383d5e742a5bca6a5d6da
beyChill <74477178+beyChill@users.noreply.github.com>
2025-05-09 22:23:40 -0400

    refactor: performance tweeks / list to sets

--- stardust/apps/chaturbate/api_streamer_bio.py
+++ stardust/apps/chaturbate/api_streamer_bio.py
@@ -2,69 +2,67 @@ import asyncio
 import re
 from datetime import datetime
 
-from stardust.apps.chaturbate.db_query import query_bio
-from stardust.apps.chaturbate.db_write import (
-    write_api_data,
-    write_m3u8,
-    write_videocontext_fail,
-)
+from stardust.apps.chaturbate.db_chaturbate import DbCb
 from stardust.apps.chaturbate.handleurls import NetActions
-from stardust.apps.chaturbate.models import ChatVideoContext
-from stardust.apps.manage_app_db import HelioDB
-from stardust.config.constants import FailVideoContext
+from stardust.apps.chaturbate.models import ChatVideoContext, FailVideoContext
 from stardust.utils.applogging import HelioLogger, loglvl
-# from stardust.utils.general import process_cb_hls
 from stardust.utils.handle_m3u8 import HandleM3u8
 from stardust.utils.timer import AppTimer
 
 log = HelioLogger()
 processed: list[str] = []
-db= HelioDB('chaturbate')
+db = DbCb("chaturbate")
+
+iNet = NetActions()
 
 
 @AppTimer
-async def manage_api_videocontext(streamers: list[str]):
-    iNet = NetActions()
+async def query_api_videocontext(streamers: list[str]):
     results = await iNet.get_all_bio(streamers)
 
-    accessible = [data for data in results if isinstance(data, ChatVideoContext)]
-    fail = [data for data in results if isinstance(data, FailVideoContext)]
+    return results
 
-    if accessible:
-        has_hls = handle_response(accessible)
-        hls_urls = await iNet.get_all_m3u8(has_hls)
-        # streamer_url = process_cb_hls(hls_urls) 
-        streamer_url= [(name_, str(HandleM3u8(url).new_cb_m3u8)) for name_, url in hls_urls]
-        db.write_capture_url(streamer_url)
+
+async def handle_results(results: list[FailVideoContext | ChatVideoContext]):
+    success = {data for data in results if isinstance(data, ChatVideoContext)}
+
+    fail = {data for data in results if isinstance(data, FailVideoContext)}
+
+    if not success:
+        log.error("Chaturbate calls to videocontext api failed")
+        return None
 
     if fail:
-        write_videocontext_fail(fail)
+        db.write_videocontext_fail(fail)
+        return None
 
-    log.app(loglvl.SUCCESS, f"Completed {len(accessible)} of {len(streamers)} queries")
-    return len(accessible) + len(fail)
+    profile_data = process_json(success)
+    db.write_api_data(profile_data)
+    has_m3u8 = chk_m3u8(success)
 
+    streamers_online = [
+        (HandleM3u8(url).new_cb_m3u8(), name_) for name_, url in has_m3u8
+    ]
 
-def handle_response(results):
-    full_json, has_hls = process_response(results)
+    db.write_capture_url(streamers_online)
 
-    profile_data = process_json(full_json)
-    write_api_data(profile_data)
-
-    return has_hls
-
-
-def process_response(results):
-    full_json = []
-    hls_url: list[str] = []
-    for result in results:
-        full_json.append(ChatVideoContext(**result))
-        if result["hls_source"]:
-            hls_url.append(result["hls_source"])
-
-    return full_json, hls_url
+    log.debug(
+        f"Success: {len(success)} / fail: {len(fail)} || {len(results)} CB api videocontext queries"
+    )
+    return streamers_online
 
 
-def process_json(json_: list[ChatVideoContext]):
+def chk_m3u8(results: set[ChatVideoContext]):
+    hls_url = {
+        (result.broadcaster_username, result.hls_source)
+        for result in results
+        if result.hls_source
+    }
+
+    return hls_url
+
+
+def process_json(json_: set[ChatVideoContext]):
     sql_data = []
     last_broadcast = None
 
@@ -99,19 +97,21 @@ def handle_hashtags(text: str):
     return hashtag_to_str
 
 
-async def videocontext():
-    streamers = query_bio()
+async def manage_api_query():
+    if not (streamers := db.query_bio()):
+        log.warning("Zero CB steamers need bio update")
+        return None
 
     names = [streamer[0] for streamer in streamers]
 
     if len(names) == 0:
         return None
 
-    streamers_online = await manage_api_videocontext(names)
-
+    results = await query_api_videocontext(names)
+    streamers_online = await handle_results(results)
     api_fail = [item for item in names if item not in processed]
 
-    log.app(loglvl.SUCCESS, f"{len(api_fail)} of {len(names)} queries")
+    log.app(loglvl.SUCCESS, f"{len(streamers) - len(api_fail)} of {len(names)} queries")
     if not streamers_online:
         return None
 
@@ -119,4 +119,4 @@ async def videocontext():
 
 
 if __name__ == "__main__":
-    asyncio.run(videocontext())
+    asyncio.run(manage_api_query())
