3fc7b15934c3ed7b585bb387f4944cecca8ef69b
beyChill <74477178+beyChill@users.noreply.github.com>
2025-05-09 22:35:32 -0400

    deleted/moved/renamed

--- stardust/config/arg_parser.py
+++
@@ -1,9 +0,0 @@
-from cmd2 import Cmd2ArgumentParser
-
-def get_parser():
-    cli_get_cmd = Cmd2ArgumentParser(description='capture streamer or show data')
-    cli_get_cmd.add_argument("streamer", nargs="?", help="Streamer name", default="--capture")
-    cli_get_cmd.add_argument("-c", "--capture", action="store_true", help="Capture streamer")
-    cli_get_cmd.add_argument("-d", "--data", action="store_true", help="Display streamer data")
-
-    return cli_get_cmd
