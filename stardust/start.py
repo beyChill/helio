217d3a51b7c3cc88f4f82cc1d46d28258f35f03d
beyChill <74477178+beyChill@users.noreply.github.com>
2025-05-20 00:07:50 -0400

    feat: new loop for automated myfreecam model status

--- stardust/start.py
+++ stardust/start.py
@@ -4,6 +4,7 @@ from threading import Thread
 from stardust.apps.chaturbate.api_get_jsons import loop_cb_all_online
 from stardust.apps.chaturbate.api_streamer_online import loop_cb_seek_capture
 from stardust.apps.myfreecams.api_get_jsons import loop_mfc_all_online
+from stardust.apps.myfreecams.api_streamer_online import loop_mfc_seek_capture
 from stardust.cli.commandline import HelioCli
 from stardust.database.db_base import db_init
 
@@ -11,12 +12,13 @@ threads = {
     Thread(target=loop_cb_all_online, daemon=True),
     Thread(target=loop_cb_seek_capture, daemon=True),
     Thread(target=loop_mfc_all_online, daemon=True),
+    Thread(target=loop_mfc_seek_capture, daemon=True),
 }
 
 
 def main():
     db_init()
-    # run_threads()
+    run_threads()
     cli = HelioCli()
     sys.exit(cli.cmdloop())
 
