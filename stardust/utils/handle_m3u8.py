0c7904fa021a41754ace2ce50aacbad14c7abf2f
beyChill <74477178+beyChill@users.noreply.github.com>
2025-05-23 10:30:12 -0400

    refactor: imporved get request error handling. Function for mfc failures.

--- stardust/utils/handle_m3u8.py
+++ stardust/utils/handle_m3u8.py
@@ -1,6 +1,8 @@
 import m3u8
 from rnet import Client, Response
 
+from stardust.apps.myfreecams.helper import MFC_VIDEO_STATUS
+from stardust.apps.myfreecams.json_models import UserApi
 from stardust.utils.applogging import HelioLogger
 
 log = HelioLogger()
@@ -17,10 +19,16 @@ class HandleM3u8:
 
     async def get_playlist(self):
         resp: Response = await self.client.get(self.url)
-        if resp.status!=200:
-            log.failure(f"HandleM3u8 {self.url}")
+
+        if resp.status == 403:
             return
+
+        if resp.status != 200:
+            log.failure(f"HandleM3u8: {resp.status} {self.url}")
+            return
+
         self.text = await resp.text()
+        return self.text
 
     def get_m3u8(self):
         self.m3u8 = self.load()
@@ -48,9 +56,20 @@ class HandleM3u8:
 
         return top_bw.uri
 
+    async def get_current_status(self):
+        """Extract user ID from the url. Then use the\n
+        user api to gain current streamer status."""
+        partial = self.url.rsplit(".f4v_")[0][-8:]
+        uid = int(partial.lstrip())
+        resp: Response = await self.client.get(f"https://app.myfreecams.com/user/{uid}")
+        json_ = await resp.json()
+        data = UserApi(**json_)
+        status = MFC_VIDEO_STATUS.get(data.vs, "UNKNOWN")
+        log.info(f"{data.username} [MFC] is {status}")
+
     async def cb_m3u8(self):
         if await self.get_playlist() is None:
-            return 
+            return
         bw = self.get_m3u8()
         new_m3u8: str = self.url.replace("playlist.m3u8", str(bw))
         streamer_name: str = new_m3u8.rsplit("-sd-")[0].split("amlst:")[1]
@@ -58,9 +77,9 @@ class HandleM3u8:
 
     async def mfc_m3u8(self):
         if await self.get_playlist() is None:
+            await self.get_current_status()
             return
         bw = self.get_m3u8()
         split_m3u8 = self.url.split("/").pop()
         new_url = self.url.replace(split_m3u8, str(bw))
         return new_url
-
