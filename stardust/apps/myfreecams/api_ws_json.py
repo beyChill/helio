3cf8320f744a2618901ee41eb0a54def0f55addb
beyChill <74477178+beyChill@users.noreply.github.com>
2025-06-12 07:16:08 -0400

    refactor: streamlined and code dependency elimination

--- stardust/apps/myfreecams/api_ws_json.py
+++ stardust/apps/myfreecams/api_ws_json.py
@@ -6,7 +6,6 @@ import re
 import time
 from urllib.parse import unquote
 
-import pandas as pd
 from rnet import Message, WebSocket, websocket
 
 import stardust.utils.heliologger as log
@@ -17,37 +16,32 @@ from stardust.utils.general import script_delay
 
 
 async def send_message(ws):
-    await ws.send(Message.from_text("fcsws_20180422\n"))
-    await ws.send(Message.from_text("1 0 0 20071025 0 1/guest:guest\n"))
+    await ws.send(Message.from_text("fcsws_20180422\n\0"))
+    await ws.send(Message.from_text("1 0 0 20071025 0 1/guest:guest\n\0"))
 
 
 async def receive_message(ws: WebSocket, server):
     url_params = ""
-    arg1 = 0
     count_loop_iterations = 0
+
     async for socket in ws:
         count_loop_iterations += 1
-        chat_data = socket.text.split("%")[0][6:]
-        chat_extdata = chat_data[:2]
+        target_data = socket.text.split()[0]
 
-        # 81 contains the data required to build url for api data
-        # which provides all online streamers
-        if chat_extdata != "81":
+        # sockets ending 81 contain the data required to build url for api call
+        # This url will provide json of all online streamers
+        if target_data not in ["00025481", "00025581", "00025681", "00025781"]:
             continue
 
         clean_text = unquote(socket.text)
-        separate_json = re.search("({.+})", clean_text)
-        if not separate_json:
+
+        if not (separate_json := re.search("({.+})", clean_text)):
             continue
 
         url_params = json.loads(separate_json.group(0))
         log.debug(url_params)
-        # sometimes 81 presents a zero value for arg1
-        # In this case, keep checking sockets
-        arg1 = url_params["msg"]["arg1"]
-        if arg1 > 0:
-            log.debug(f"{count_loop_iterations=}")
-            break
+        log.debug(f"{count_loop_iterations=}")
+        break
 
     if not url_params:
         return
@@ -56,6 +50,7 @@ async def receive_message(ws: WebSocket, server):
     key = url_params["respkey"]
     serv = url_params["serv"]
     type_ = url_params["type"]
+    arg1 = url_params["msg"]["arg1"]
     arg2 = url_params["msg"]["arg2"]
 
     api_url = f"https://www.myfreecams.com/php/FcwExtResp.php?host={server}&respkey={key}&type={type_}&opts={opts}&serv={serv}&arg1={arg1}&arg2={arg2}&owner=0&nc=2915592&debug=cams"
@@ -65,21 +60,21 @@ async def receive_message(ws: WebSocket, server):
 
 
 async def get_socket_api_url():
-    """The api url expires relatively quickly"""
+    """Gather data from websockets to generate live url which expires relatively quickly"""
+    api_url = ""
     server = random.choice(WS_SERVERS)
     host = f"wss://{server}.myfreecams.com/fcsl"
-    api_url = ""
     ws = await websocket(host)
 
-    if ws.ok:
-        send_task = asyncio.create_task(send_message(ws))
-        receive_task = asyncio.create_task(receive_message(ws, server))
+    async with ws:
+        if ws.ok:
+            send_task = asyncio.create_task(send_message(ws))
+            receive_task = asyncio.create_task(receive_message(ws, server))
 
-        _, api_url = await asyncio.gather(send_task, receive_task)
-        
-        await ws.close()
-        send_task.cancel()
-        receive_task.cancel()
+            try:
+                _, api_url = await asyncio.gather(send_task, receive_task)
+            except Exception as e:
+                log.error(e)
 
     return api_url
 
@@ -89,19 +84,16 @@ def handle_streamers_online(json_):
 
     log.info(f"{json_['count']} MFC streamers online")
     data = json_["rdata"]
-    df = pd.DataFrame((data[1:]))
 
-    # Not sure of simple method to include all data from api (63 columns)
-    # keeping most valuable colums vs all columns
-    df_url = df.iloc[:, :8]
-    url_data: list = df_url.values.tolist()
+    # populate table with only data used for m3u8 creation
+    # ["nm", "sid", "uid", "vs", "pid", "lv", "camserv", "phase"]
+    url_data = [x[0:8] for x in data[1:-1]]
 
-    # Again just keeping valued data from the 63 items the api provides
-    other_data = df.loc[0:, [0, 11, 16, 17, 18, 19, 20, 21, 22]]
-
-    # the dataframe to sql function (tosql) overwrites column heads in table.
-    # converting it to a list eliminates issue.
-    streamer_data: list = other_data.values.tolist()
+    # api returns tons of data Helio doesn't use. Picking and
+    # choosing the desired data columns
+    # ["nm", "creation" "new_model", "missmfc", "camscore", "continent", "flags", "rank", "rc"]
+    # i.e. ['ArtemisArte', 1725448438, 0, 0, 78.2, 'EU', 33897504, 0, 4]
+    streamer_data = [[x[0], x[11], *x[16:23]] for x in data[1:-1]]
 
     db.write_streamer_data(streamer_data)
     db.write_url_data(url_data)
@@ -116,7 +108,7 @@ def manage_websocket():
         json_ = asyncio.run(iNet.get_all_online_streamers(api_url))
         handle_streamers_online(json_)
 
-        delay_, time_ = script_delay(302.47, 425.389)
+        delay_, time_ = script_delay(17.47, 21.389)
         log.query(f"MFC streamers @ {time_}")
         time.sleep(delay_)
 
